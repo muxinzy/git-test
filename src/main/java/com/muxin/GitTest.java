ddc30e984cdb4d7a491c271cac4608d1b206c4a2
muxinzy <308746163@qq.com>
2022-11-26 21:23:56 +0800

    first commit

---
+++ src/main/java/com/muxin/GitTest.java
@@ -0,0 +1,13 @@
+package com.muxin;
+
+/**
+ * @PROJECT_NAME: git-test
+ * @DESCRIPTION:
+ * @USER: muxin
+ * @DATE: 2022/11/26 21:07
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
